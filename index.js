2b751129f1fb58ada278ea67e9d84f05860d9cfa
Dhruv1701 <negidhruv1701@gmail.com>
2022-09-18 03:11:56 +0530

    added server start

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+app.get("/",(req,res)=>{
+    res.json("server start")
+})
+
 app.use('/api/authen', require('./routes/authen'))
 app.use('/api/note', require('./routes/note'))
 
