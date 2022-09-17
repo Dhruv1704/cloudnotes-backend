ca70249a21956e9f15b29bcb765145cd4a0fbfa9
Dhruv1701 <negidhruv1701@gmail.com>
2022-09-18 03:06:54 +0530

    Deploying cloudnotes server

---
+++ index.js
@@ -0,0 +1,19 @@
+const connectToMongo = require("./db");
+const express = require('express')
+var cors = require('cors')
+
+
+connectToMongo()  // asynchronous
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/authen', require('./routes/authen'))
+app.use('/api/note', require('./routes/note'))
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
